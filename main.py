bb3400a671096e5ea652fdd56645f8a3e14f8138
NussNougatt <nibin100@hhu.de>
2026-04-05 18:45:52 +0900

    Front and reverse cards are created. Furigana are available. The format problem has been resolved. Also it now automatically creates the deck in Anki

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import deepl
 import genanki
+import requests
+import os
+
+from deepl import version
+
+my_css = """
+.card {
+    font-family: arial;
+    font-size: 20px;
+    text-align: center;
+    color: black;
+    background-color: white;
+}
+
+/* This creates a container that fills the screen and centers its contents */
+.centered-content {
+    display: flex;
+    justify-content: center; /* Horizontal center */
+    align-items: center;     /* Vertical center */
+    height: 100vh;           /* Full viewport height */
+}
+"""
 
 my_model = genanki.Model(
     1234567890,
@@ -10,17 +32,35 @@ my_model = genanki.Model(
     ],
     templates=[
         {
-        'name': 'Card 1',
+        'name': 'Front_Card',
         'qfmt': '''{{kanji:Front}}''',
         'afmt': '{{furigana:Front}}<hr id=answer>{{kanji:Back}}',
         },
+        {
+        'name': 'Reverse_Card',
+        'qfmt': '''{{kanji:Back}}''',
+        'afmt': '{{{kanji:Back}}<hr id=answer>{{furigana:Front}}',
+        },
     ],
+    css=my_css,
 )
+
+def auto_import(path):
+    payload = {
+        "action": "importPackage",
+        "version": 6,
+        "params": { "path": path }
+    }
+    try:
+        response = requests.post("http://localhost:8765", json=payload)
+        print("Import Success:", response.json())
+    except Exception as e:
+        print("Error connecting to Anki:", e)
 
 
 my_note = genanki.Note(
     model=my_model,
-    fields=['定義する', 'definieren']
+    fields=['定義する[ていぎ]', 'definieren']
 )
 
 my_deck = genanki.Deck(
@@ -31,3 +71,6 @@ my_deck = genanki.Deck(
 my_deck.add_note(my_note)
 
 genanki.Package(my_deck).write_to_file('japanese_deck.apkg')
+
+package_path = os.path.abspath("japanese_deck.apkg")
+auto_import(package_path)
